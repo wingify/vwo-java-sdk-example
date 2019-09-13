89e65e6e225e183f1532561e42cd74a32c468ce6
pntgupta <punit.gupta14@yahoo.com>
2019-09-13 13:22:19 +0530

    feat(app): Add feature test and rollout support SST-73

--- src/main/java/com/example/demo/config/Config.java
+++ src/main/java/com/example/demo/config/Config.java
@@ -5,4 +5,14 @@ public class Config {
   public static String sdkKey = "REPLACE_THIS_WITH_CORRECT_VALUE";
   public static String campaignTestKey = "REPLACE_THIS_WITH_CORRECT_VALUE";
   public static String goalIdentifier  = "REPLACE_THIS_WITH_CORRECT_VALUE";
+
+  public static String featureRolloutCampaignKey = "FEATURE_ROLLOUT_KEY";
+  public static String featureTestCampaignKey = "FEATURE_TEST";
+  public static String featureTestGoalIdentifier = "FEATURE_TEST_GOAL";
+  public static Object featureTestRevenue = null;
+
+  public static String stringVariableKey = "STRING_VARIABLE";
+  public static String integerVariableKey = "INTEGER_VARIABLE";
+  public static String doubleVariableKey = "DOUBLE_VARIABLE";
+  public static String booleanVariableKey = "BOOLEAN_VARIABLE";
 }
